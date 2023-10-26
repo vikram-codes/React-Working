a7aab6145dc5099647f99b56d53a5f1f27ac7468
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-26 19:01:13 +0530

    Setting up the devopment Environment

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ function TabContent({ item }) {
 function DifferentContent() {
   return (
     <div className="tab-content">
-      <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
+      <h4>I&apos;m a DIFFERENT tab, so I reset state 💣💥</h4>
     </div>
   );
 }
